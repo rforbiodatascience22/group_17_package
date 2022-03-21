ae452e895a3e4a1d68ab548e617c467d0837f705
elena-krismer <70535771+elena-krismer@users.noreply.github.com>
2022-03-21 16:25:50 +0100

    add rna codon table

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' RNA Codons
+#'
+#' List with RNA codons encoding Amino Acids
+#'
+#' @format
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"aa_codons"
