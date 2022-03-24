ee17241166e4369fad5606de6813416191f70b7c
narong-narnua <narnua@food.dtu.dk>
2022-03-24 10:36:40 +0000

    add transcribe

---
+++ R/transcribe.R
@@ -0,0 +1,12 @@
+#' Title Transcription
+#'
+#' @param DNA_seq
+#'
+#' @return mRNA
+#' @export
+#'
+#' @examples transcribe ("ATCG")
+transcribe <- function(DNA_seq){
+  mRNA <- gsub("T", "U", DNA_seq)
+  return(mRNA)
+}
